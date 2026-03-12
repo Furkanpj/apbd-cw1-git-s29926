4d1be14c3a5094cf901c7c5543f58b71ec362caa
Furkan Senci <s29926@pjwstk.edu.pl>
2026-03-12 19:48:41 +0100

    Change startup message on main

--- apbd-cw1-git-s29926/Program.cs
+++ apbd-cw1-git-s29926/Program.cs
@@ -1,6 +1,6 @@
 ﻿using apbd_cw1_git_s29926;
 
-Console.WriteLine("Statistics application started.");
+Console.WriteLine("Statistics application started - main branch version.");
 Console.WriteLine("Enter numbers separated by commas:");
 string? input = Console.ReadLine();
 
