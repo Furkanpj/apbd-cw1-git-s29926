c64213107babd8931a1cc5eb04dfdd383b47bffd
Furkan Senci <s29926@pjwstk.edu.pl>
2026-03-12 19:39:47 +0100

    Add CalculateAverage method

--- apbd-cw1-git-s29926/StatisticsHelper.cs
+++ apbd-cw1-git-s29926/StatisticsHelper.cs
@@ -1,6 +1,12 @@
 ﻿namespace apbd_cw1_git_s29926;
 
-public class StatisticsHelper
+public static class StatisticsHelper
 {
-    
+    public static double CalculateAverage(int[] values)
+    {
+        if (values.Length == 0)
+            return 0;
+
+        return values.Average();
+    }
 }
